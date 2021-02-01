0b22f042afa180e85fb34cc3772d1fed26f10aad
github username <github email address>
2021-02-01 08:16:56 -0500

    done and working

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,7 +1,11 @@
 import React from "react";
-import whoseHouse from "./houses/HagridsHouse";
+import whoseHouse from "./houses/whoseHouse";
 import HooflePoof from "./houses/Hufflepuff";
+
+// these below all do the same thing
 import * as GryffFunctions from "./houses/Gryffindor";
+// import { colors, gryffMascot } from "./houses/Gryffindor"; // able to reference the variable imported by its previously declared name:
+// import { colors, gryffMascot as mascot } from "./houses/Gryffindor"; //rename it inside of our import statement:
 
 GryffFunctions.colors();
 // => 'Scarlet and Gold'
